49eadec1c592535d492cbce6644fd694530e41e8
thegears <semh.1362@gmail.com>
2023-11-07 16:25:32 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import auth from "auth-astro";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), auth()],
   output: "server",
-  adapter: node({
-    mode: "hybrid"
-  })
+  adapter: vercel()
 });
